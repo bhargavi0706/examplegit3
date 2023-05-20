fdda70af8d452ef0ed03808b3f993660a0113591
bhargavi0706 <rajuldevbhargavi@gmail.com>
2023-05-20 12:39:03 +0530

    basic

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Form</title>
+    <!--CSS-->
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+</head>
+<body>
+    <div class="container">
+        <form class="form" id="login">
+            <h1 class="form_title">Login</h1>
+            <div class="form_message form_message-error"></div>
+            <div class="form_input-group">
+                <input type="text" class="form_input" autofocus placeholder="Username or email">
+                <div class="form_input-error-msg"></div>
+            </div>
+            <div class="form_input-group">
+                <input type="password" class="form_input" id="id_password"  autofocus placeholder="Password">
+                <i class="bi bi-eye-slash" id="togglePassword" ></i>
+                <div class="form_input-error-msg"></div>
+            </div>
+
+            <div class="form-group">        
+                <div class="checkbox">
+                    <label><input type="checkbox" name="remember"> Remember me</label>
+                </div>
+            </div>
+
+            <p class="form_textP">
+                <a href="#" class="form_link">Forgot your password?</a>
+            </p>
+            <button class="form_button" type="submit">login</button>
+            
+            <p class="form_text">
+                <span>Don't have an account?<a class="form_link" href="./" id="linkCreateAccount">Create account</a></span>
+            </p>
+            
+        </form>
+        <form class="form form-hidden" id="createAccount">
+            <h1 class="form_title">Create Account</h1>
+            <div class="form_message form_message-error"></div>
+            <div class="form_input-group">
+                <input type="text" id="signupUsername" class="form_input" autofocus placeholder="Username">
+                <div class="form_input-error-msg"></div>
+            </div>
+            <div class="form_input-group">
+                <input type="text" class="form_input" autofocus placeholder="Email Address">
+                <div class="form_input-error-msg"></div>
+            </div>
+            <div class="form_input-group">
+                <input type="password" class="form_input-password form_input" id="inp_password" autofocus placeholder="Password">
+                <div class="form_input-error-msg" ></div>
+            </div>
+            <div class="form_input-group">
+                <input type="password" class="form_input-password form_input"  autofocus placeholder="Confirm password">
+                <div class="form_input-error-msg"></div>
+            </div>
+            <button class="form_button" type="submit">Continue</button>
+            <p class="form_text">
+                <span>Already have an account?<a class="form_link" href="./" id="linkLogin">Sign in</a></span>
+            </p>
+        </form>
+
+        <!--
+        <div class="row">
+            <label>Username</label>
+            <div class="input-group">
+              <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
+              <input type="text" class="form-control" placeholder="First name" required>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <label>First Name</label>
+            <div class="input-group">
+              <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
+              <input type="text" class="form-control" placeholder="First name" required>
+            </div>
+          </div>
+          <div class="col">
+            <label>Last Name</label>
+            <div class="input-group">
+              <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
+              <input type="text" class="form-control" placeholder="First name" required>
+            </div>
+          </div>
+        </div>-->
+    </div>
+    <script src="main.js"></script>
+
+</body>
---
+++ main.js
@@ -0,0 +1,63 @@
+//clicking on login button
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form_message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form_message-success", "form_message-error");
+    messageElement.classList.add(`form_message-${type}`);
+}
+//type wrong credentials
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-msg").textContent = message;
+}
+//typing credentials displaying error msg
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-msg").textContent = "";
+}
+//to hide signup while login viceversa
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    //hiding singup form
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
+    });
+    //hiding login form
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
+    });
+    //error msg -> directly clicking on login button without entering credentials
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+    //diplay error msg because of incoorect credentials in username signup page
+    document.querySelectorAll(".form_input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+
+//eye-icon
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#id_password');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('bi-eye');
+});
