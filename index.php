b140a142d7ff9e3554b7b538c44f60137ea251a7
bhargavi0706 <rajuldevbhargavi@gmail.com>
2023-05-25 17:45:14 +0530

    aft_bp_nch

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         <title>Form</title>
-        <!--CSS-->
+        <!--CSS styling-->
         <link rel="stylesheet" href="main.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     </head>
